c1ed01cd70822f4ba8b33a7e0ec65d72ae4538dc
Sergei Fomin <sergeifomin83@inbox.ru>
2020-08-06 19:23:49 +0300

    changed public path for gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   output: {
     filename: 'js/[name].js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    publicPath: '/FSD-Toxin/'
   },
 
   devServer: {
