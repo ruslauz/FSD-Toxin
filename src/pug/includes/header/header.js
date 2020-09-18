f64feac6f65f60783d23df2643ca44027782d3f0
Sergei Fomin <sergeifomin83@inbox.ru>
2020-09-18 21:43:14 +0300

    small bugfixes

--- src/pug/includes/header/header.js
+++ src/pug/includes/header/header.js
@@ -1,8 +1,10 @@
 const burgerButton = document.querySelector('.js-burger-button');
 const mobileMenu = document.querySelector('.js-mobile-menu')
 
-burgerButton.addEventListener('click', e => {
-  e.target.classList.toggle('header__burger-button_opened')
-  mobileMenu.classList.toggle('header__mobile-menu_opened')
-  document.body.classList.toggle('fixed')
-})
+if (burgerButton) {
+  burgerButton.addEventListener('click', e => {
+    e.target.classList.toggle('header__burger-button_opened')
+    mobileMenu.classList.toggle('header__mobile-menu_opened')
+    document.body.classList.toggle('fixed')
+  })
+}
